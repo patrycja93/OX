d7ee5995832798602140f317ef5dd67d3f9d5c7d
patka <patrycja.hyjek@gmail.com>
2019-12-20 18:27:35 +0100

    Created players.

---
+++ src/main/java/pl/patrycja/ox/Player.java
@@ -0,0 +1,11 @@
+package pl.patrycja.ox;
+
+public class Player {
+    private String name;
+    private Sign sign;
+
+    public Player(String name, Sign sign) {
+        this.name = name;
+        this.sign = sign;
+    }
+}
