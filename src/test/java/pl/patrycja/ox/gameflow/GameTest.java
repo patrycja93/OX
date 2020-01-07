615418724926d02c6d29f87ccf32badb9d58af3b
Patrycja Hyjek <patrycja_hyjek@epam.com>
2020-01-07 12:52:19 +0100

    Added tests for Game class.

--- src/test/java/pl/patrycja/ox/gameflow/GameTest.java
+++ src/test/java/pl/patrycja/ox/gameflow/GameTest.java
@@ -3,21 +3,23 @@ package pl.patrycja.ox.gameflow;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
 import pl.patrycja.ox.Player;
+import pl.patrycja.ox.Sign;
 import pl.patrycja.ox.ui.UI;
 
 import java.util.List;
 
-import static org.testng.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.*;
 
-public class AutomaticTestsTest {
+public class GameTest {
 
     UI ui = Mockito.mock(UI.class);
 
     @Test
     public void settingsShouldSetBoardSizeToFive() {
         //given
-        String[] args = {"5", "4", "0"};
-        Mode mode = new AutomaticTests(ui);
+        String[] args = {"5", "4"};
+        Mode mode = new Game(ui);
 
         //when
         mode.settings(args);
@@ -29,8 +31,8 @@ public class AutomaticTestsTest {
     @Test
     public void settingsShouldSetUnbrokenLineToFour() {
         //given
-        String[] args = {"5", "4", "0"};
-        Mode mode = new AutomaticTests(ui);
+        String[] args = {"5", "4"};
+        Mode mode = new Game(ui);
 
         //when
         mode.settings(args);
@@ -42,51 +44,59 @@ public class AutomaticTestsTest {
     @Test
     public void settingsShouldSetNumberOfMatches() {
         //given
-        String[] args = {"5", "4", "0"};
-        Mode mode = new AutomaticTests(ui);
+        String[] args = {"5", "4"};
+        Mode mode = new Game(ui);
 
         //when
         mode.settings(args);
 
         //then
-        assertEquals(mode.gameSettings.getNumberOfMatches(), 10);
+        assertEquals(mode.gameSettings.getNumberOfMatches(), 3);
     }
 
     @Test
-    public void settingsShouldSetNumberOfMatchesDiagonals() {
+    public void settingsShouldSetNumberOfUnbrokenLineToThree() {
         //given
-        String[] args = {"5", "4", "3"};
-        Mode mode = new AutomaticTests(ui);
+        String[] args = {"5"};
+        Mode mode = new Game(ui);
 
         //when
         mode.settings(args);
 
         //then
-        assertEquals(mode.gameSettings.getNumberOfMatches(), 4);
+        assertEquals(mode.gameSettings.getNumberOfMatches(), 3);
     }
 
     @Test
-    public void settingsShouldSetNumberOfMatchesDraw() {
+    public void createPlayersShouldCreatedTwoPlayersWithDifferentSign() {
         //given
-        String[] args = {"5", "4", "4"};
-        Mode mode = new AutomaticTests(ui);
+        Mode mode = new Game(ui);
+        String name = "A";
+        Sign signO = Sign.O;
+        Sign signX = Sign.X;
 
         //when
-        mode.settings(args);
+        List<Player> players = mode.createPlayers();
+        when(ui.read()).thenReturn(name);
 
         //then
-        assertEquals(mode.gameSettings.getNumberOfMatches(), 1);
+        assertEquals(players.get(0).getSign(), signX);
+        assertEquals(players.get(1).getSign(), signO);
+        assertEquals(players.size(), 2);
     }
 
     @Test
     public void createPlayersShouldReturnListOfPlayersEqualsTwo() {
         //given
-        Mode mode = new AutomaticTests(ui);
+        Mode mode = new Game(ui);
+        String name = "A";
 
         //when
         List<Player> players = mode.createPlayers();
+        when(ui.read()).thenReturn(name);
 
         //then
         assertEquals(players.size(), 2);
     }
+
 }
